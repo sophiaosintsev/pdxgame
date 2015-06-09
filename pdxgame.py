8d32fd5444fc9e9ee939eccb9bbb39299780e78c
sophiaosintsev <sofica05@gmail.com>
2015-06-08 21:21:04 -0700

    Added more game functionality and ability to win/lose.

--- pdxgame.py
+++ pdxgame.py
@@ -46,8 +46,8 @@ def tile_change(tile):
     for x in tile.exits.keys():
         tiles += x + ' '
     try:
-        tile_choice = raw_input("What direction do you choose? %s ? > " % tiles)
-        play(tile.exits[tile_choice])
+        tile_choice = raw_input("Where do you want to go? %s > " % tiles)
+        move(tile.exits[tile_choice], player)
     except KeyError:
         print "I do not understand that."
         tile_change(tile)
@@ -56,9 +56,9 @@ def tile_change(tile):
 
 def randomizer(player):
 
-    response = [ "Rain", "Canvasser", "Naked Bike Ride", "Bike Messengers", "Rush Hour",
-                 "Free Box", "Freak Sunshine", "Coffee and Donuts", "Thrift Store Find",
-                 "Free Hugs"]
+    response = [ "Rain", "a Canvasser", "the Naked Bike Ride", "Bike Messengers", "Rush Hour",
+                 "a Free Box", "Freak Sunshine", "Coffee and Donuts", "a Thrift Store Find",
+                 "Free Hugs" ]
     response_num = random.randint(0,9)
     if response_num > 4:
         player.increase_hp(10)
@@ -73,34 +73,52 @@ def choose_player():
                                 1 for vegan
                                 2 for panhandler
                                 3 for bearded techie """)
-    player_name = raw_input("And what do you like to be called?")
+    player_name = raw_input("And what do you like to be called? ")
     return player_type, player_name
 
 def move(tile, player):
-    print "You are in Portland."
     obstacle = randomizer(player)
-    print obstacle
+    print "Guess what? You ran into " + obstacle + ". How Portland!"
     tile_change(tile)
 
 player_type, player_name = choose_player()
 
 if player_type == "0":
     player = Hipster(player_name)
+    chosen_type = "Hipster"
 
 elif player_type == "1":
     player = Vegan(player_name)
+    chosen_type = "Vegan"
 
 elif player_type == "2":
     player = Panhandler(player_name)
+    chosen_type = "Panhandler"
 
 elif player_type == "3":
     player = BeardedTechie(player_name)
+    chosen_type = "Bearded Techie"
 
 else:
     print "Sorry, there are no other options."
 
+print "Hi, %s. Isn't it great to be a %s?" % (player_name, chosen_type) + " You are starting with " + str(player.hp) + " hipster points."
+
 tile1 = Tile( {} )
 tile2 = Tile( {} )
-tile1.exits = {"Forward": tile2}
-tile2.exits = {"Back": tile1}
+tile3 = Tile( {} )
+tile4 = Tile( {} )
+tile5 = Tile( {} )
+tile1.exits = {"NE": tile5}
+tile2.exits = {"N": tile1}
+tile3.exits = {"SE": tile2}
+tile4.exits = {"SW": tile3}
+tile5.exits = {"NW": tile4}
 move(tile1, player)
+
+if player.hp == 150:
+    print "You are the ultimate Portlander and YOU WIN!"
+    quit()
+elif player.hp == 80:
+    print "Sorry, you lose. Go back to California."
+    quit()
